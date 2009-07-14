62d13e1d78919fe03f2c2f8a0fdf87cbf9681f0e
lijie <lijay@126.com>
2009-07-14 10:19:49 +0800

    mapping 32M size ram

--- kernel/boot.c
+++ kernel/boot.c
@@ -38,7 +38,7 @@ static inline void move_kernel_code(unsigned long from)
 	memcpy((void *)_text - __PAGE_OFFSET, (void *)from, size);
 }
 
-static void default_page_map(void)
+static void default_page_map(size_t memsize)
 {
 	unsigned long *pde = (unsigned long *)pg;
 	unsigned long *pte;
@@ -51,13 +51,11 @@ static void default_page_map(void)
 	/* pde needs 4K */
 	pte = pde + 1024;
 
-	/* mapping 8M size */
+	/* mapping 32M size */
 	j = 0;
-	for (i = 0; i < (0x800000 >> PDE_SHIFT); i++) {
+	for (i = 0; i < (0x2000000 >> PDE_SHIFT); i++) {
 		pde[i] = (unsigned long)(pte + i * 1024) + attr;
-//		pde[i] = (unsigned long)pte;
 		pde[(__PAGE_OFFSET / (1 << PDE_SHIFT)) + i] = pde[i];
-//		pde[0x300 + i] = pde[i];
 		for (;j < i * 1024; j++) {
 			pte[j] = PAGE_SIZE * j + attr;
 		}
@@ -75,12 +73,38 @@ static void default_page_map(void)
 		);
 }
 
+#define	__read_disk(to, head, track, start)	\
+	__asm__ __volatile__ (			\
+		"int $0x13"						\
+		:							\
+		:"b"(to), "d"(head << 8), "c"((track << 8) | start), "a"(0x0200 | (18 - start)) \
+		)
+
+static void copy_from_disk(void)
+{
+	unsigned char sector, track, head, start;
+	unsigned char *dest = (unsigned char *)0x10000;
+
+	for (track = 0; track < 80; track++) {
+		if (track == 0)
+			start = 2;
+		else
+			start = 0;
+
+		__read_disk(dest, 0, track, start);
+		dest += 512;
+		__read_disk(dest, 1, track, start);
+		dest += 512;
+	}
+}
+
 void boot_main(unsigned long from)
 {
+//	copy_from_disk();
 	disable_NMI();
 	enable_A20();
 	mask_all_ints();
 	move_kernel_code(from);
 
-	default_page_map();
+	default_page_map(0);
 }
